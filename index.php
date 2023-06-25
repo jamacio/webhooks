0052316260c4255c470bff1d1a4feccec8273322
Jamacio <jamacio.rocha@danone.com>
2023-06-25 02:32:21 -0300

    added new deploy type

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $dir = $_GET['dir'];
 shell_exec("cd $dir");
 echo shell_exec("pwd");
-echo shell_exec("git pull origin main");
-echo shell_exec("make build");
+// echo shell_exec("git pull origin main");
+// echo shell_exec("make build");
 
 echo "done.";
